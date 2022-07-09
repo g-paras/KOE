8cc976a84a00d5546f4826ecc766a115ce30ae02
Paras Gupta <guptaparas039@gmail.com>
2022-07-09 07:20:57 +0000

    use browser router from react-router-dom

--- kietOLX-frontend/src/App.js
+++ kietOLX-frontend/src/App.js
@@ -1,13 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import Cards from './Cards';
 import Header from './Header'
- 
+import Login from './pages/Login';
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-       <Cards/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Cards />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
