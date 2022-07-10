fafb7923ba0650d2b44e5296282bee3178ae53b2
Paras Gupta <guptaparas039@gmail.com>
2022-07-10 09:42:55 +0000

    store endpoints & import them as needed

--- kietOLX-frontend/src/App.js
+++ kietOLX-frontend/src/App.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Navigate,
+  // useNavigate
 } from "react-router-dom";
 
 import './App.css';
 import Cards from './Cards';
 import Header from './Header'
 import Login from './pages/Login';
+import SignUp from "./pages/SignUp";
+import Post from "./pages/Post";
+import Attribute from "./pages/Attribute";
 
 function App() {
+  // const navigate = useNavigate();
+  const [token, setToken] = useState("");
+
+  const addToken = token => {
+    localStorage.setItem("token", token);
+    setToken(token);
+  }
+
+  const logoutUser = () => {
+    setToken("");
+    localStorage.removeItem("token");
+    // navigate('/');
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      let localToken = localStorage.getItem("token");
+      if (localToken !== null)
+        setToken(localToken);
+    };
+    fetchData();
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
+        <Header token={token} logoutUser={logoutUser} />
         <Routes>
-          <Route path="/" element={<Cards />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Cards token={token} />} />
+          <Route path="/login" element={token ? <Navigate to="/" /> : <Login addToken={addToken} />} />
+          <Route path="/signup" element={token ? <Navigate to="/" /> : <SignUp />} />
+          <Route path="/post" element={token ? <Post /> : <Navigate to="/login" />} />
+          <Route path="/post/attribute" element={token ? <Attribute /> : <Navigate to="/login" />} />
         </Routes>
       </div>
     </BrowserRouter>
