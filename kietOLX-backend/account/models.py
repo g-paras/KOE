ff1aebe2f7fa02479e42cab2b386cf52a4bdecb3
Paras Gupta <guptaparas039@gmail.com>
2022-07-18 07:00:44 +0000

    fix model field types

--- kietOLX-backend/account/models.py
+++ kietOLX-backend/account/models.py
@@ -1,10 +1,19 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+
+class Gender(models.TextChoices):
+    male = "M", "Male"
+    female = "F", "Female"
+    anonymous = "A", "Anonymous"
+
+
 class Profile(models.Model):
-    image = models.ImageField()
-    phone_no = models.PositiveIntegerField()
-    about_me = models.CharField(max_length=250)
-    gender = models.CharField(max_length=10)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    image = models.ImageField(upload_to="profile", default="profile/avatar_3.png")
+    gender = models.CharField(null=True, choices=Gender.choices, max_length=1)
+    phone_no = models.CharField(max_length=10, null=True)
+    about_me = models.CharField(default="I have something to sell", max_length=250)
+
+    def __str__(self):
+        return f"{self.user} | {self.gender}"
