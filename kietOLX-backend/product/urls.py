934e0776ab12770686750365b763ae47d843e277
Paras Gupta <guptaparas039@gmail.com>
2022-08-11 07:08:41 +0000

    add view & url for product

--- kietOLX-backend/product/urls.py
+++ kietOLX-backend/product/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import ProductRetrieveView, ProductViewSet
+from .views import (ProductCreateView, ProductRetrieveView, ProductUpdateView,
+                    ProductViewSet)
 
 router = routers.DefaultRouter()
 router.register("", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("get/<slug:slug>/", ProductRetrieveView.as_view())
+    path("get/<slug:slug>/", ProductRetrieveView.as_view()),
+    path("update/<int:pk>/", ProductUpdateView.as_view()),
+    path("mymethod/", ProductCreateView.as_view()),
 ]
--- kietOLX-backend/product/views.py
+++ kietOLX-backend/product/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, permissions, viewsets
+from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -14,3 +15,17 @@ class ProductRetrieveView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "slug"
+
+
+class ProductCreateView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=204)
+
+
+class ProductUpdateView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
