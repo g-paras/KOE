4341a752e18026ae7ab758a49ea0ce08fcd79c9e
Paras Gupta <guptaparas039@gmail.com>
2022-08-17 17:34:57 +0000

    add new routes

--- kietOLX-frontend/src/utils/constants.js
+++ kietOLX-frontend/src/utils/constants.js
@@ -1,3 +1,9 @@
 export const BASE_URL = "http://127.0.0.1:8000/";
+
 export const AUTH = "account/auth/";
 export const REGISTER = "account/user/";
+
+export const PRODUCT = "product/";
+export const PRODUCT_LIST = PRODUCT;
+export const PRODUCT_CREATE = PRODUCT + "create/";
+export const PRODUCT_GET = PRODUCT + "get/";
