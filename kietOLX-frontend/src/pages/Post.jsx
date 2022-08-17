0cedaa6f51830b4af875b7cca55497a73791f842
Paras Gupta <guptaparas039@gmail.com>
2022-08-17 17:35:50 +0000

    fix class typo

--- kietOLX-frontend/src/pages/Post.jsx
+++ kietOLX-frontend/src/pages/Post.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Post = () => {
-  const [disable, setDisable] = React.useState(true);
+  const [disable, setDisable] = useState(true);
   
   
 
@@ -13,16 +13,16 @@ const Post = () => {
     
     <div>
       
-  <ul class="cards">
-  <li  class="card12" onClick={() =>setDisable(false)} > Quantum</li>
-  <li class="card12" onClick={() => setDisable(false)} >Lan Cabel</li>
-  <li class="card12" onClick={() => setDisable(false)} >Book</li>
-  <li class="card12" onClick={() => setDisable(false)} >Notes</li>
-  <li class="card12" onClick={() => setDisable(false)} >Lab Coat</li>
-  <li class="card12" onClick={() => setDisable(false)} >ED Drafter</li>
-  <li class="card12" onClick={() => setDisable(false)} >Scientfic Calculator</li>
-  <li class="card12" onClick={() => setDisable(false)} >Cooler</li>
-  <li class="card12" onClick={() => setDisable(false)} >Others</li>
+  <ul className="cards">
+  <li  className="card12" onClick={() =>setDisable(false)} > Quantum</li>
+  <li className="card12" onClick={() => setDisable(false)} >Lan Cabel</li>
+  <li className="card12" onClick={() => setDisable(false)} >Book</li>
+  <li className="card12" onClick={() => setDisable(false)} >Notes</li>
+  <li className="card12" onClick={() => setDisable(false)} >Lab Coat</li>
+  <li className="card12" onClick={() => setDisable(false)} >ED Drafter</li>
+  <li className="card12" onClick={() => setDisable(false)} >Scientfic Calculator</li>
+  <li className="card12" onClick={() => setDisable(false)} >Cooler</li>
+  <li className="card12" onClick={() => setDisable(false)} >Others</li>
 </ul>
 
    <Link to={"/post/attribute"}><button disabled={disable} className="buttonNext"> Next</button></Link>
