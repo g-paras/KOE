a5f8bcaafeba151eb2a9fe74a14742b5021d3c9b
Paras Gupta <guptaparas039@gmail.com>
2022-08-18 14:43:30 +0000

    create view & url to get profile of logged in user

--- kietOLX-backend/account/urls.py
+++ kietOLX-backend/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import UserRetrieveView, UserViewSet
+from .views import GetUserProfile, UserRetrieveView, UserViewSet
 
 router = routers.DefaultRouter()
 router.register("user", UserViewSet)
@@ -11,4 +11,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("auth/", obtain_auth_token),
     path("get/<str:username>/", UserRetrieveView.as_view()),
+    path("my-profile/", GetUserProfile.as_view()),
 ]
--- kietOLX-backend/account/views.py
+++ kietOLX-backend/account/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, viewsets
+from rest_framework import generics, permissions, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import UserSerializer
 
@@ -14,10 +16,15 @@ class UserRetrieveView(generics.RetrieveAPIView):
     lookup_field = "username"
     queryset = User.objects.all()
 
+
+class GetUserProfile(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get(self, request, format=None):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
+
     # def get_queryset(self):
-    #     """
-    #     This view should return a list of all the purchases for
-    #     the user as determined by the username portion of the URL.
-    #     """
     #     username = self.kwargs["username"]
     #     return User.objects.get(username=username)
