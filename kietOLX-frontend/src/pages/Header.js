66ca07ee1b20d8d48616e6d206af89eca980a17d
Paras Gupta <guptaparas039@gmail.com>
2022-08-20 09:17:57 +0000

    fix images & css

--- kietOLX-frontend/src/pages/Header.js
+++ kietOLX-frontend/src/pages/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import sell from "./Images/sell.png";
-import logo from "./Images/glo.gif";
-import profile from "./Images/profile.png";
+import sell from "../images/sell.png";
+import logo from "../images/glo.gif";
+import profile from "../images/profile.png";
 import { useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
 
@@ -23,18 +23,20 @@ const Header = () => {
               className="form-control"
               placeholder="Find Quantum,Books and more...."
             />
-            <button type="button" className="btn btn-dark">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-search"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-              </svg>
-            </button>
+            <div class="input-group-append">
+              <button class="btn btn-dark" type="button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+              </button>
+            </div>
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
