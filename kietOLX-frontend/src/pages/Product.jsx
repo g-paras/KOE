1989a3028ed0df6c632384d96e10bff938803e37
Kohina <pandeykohina@gmail.com>
2022-08-28 15:48:50 +0530

    changes

--- kietOLX-frontend/src/pages/Product.jsx
+++ kietOLX-frontend/src/pages/Product.jsx
@@ -5,6 +5,8 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import moment from "moment";
+import Heart from "./Hearts";
+
 
 import { BASE_URL, PRODUCT_GET } from "../utils/constants";
 
@@ -30,23 +32,32 @@ const Product = () => {
   }, []);
 
   return (
-    <div className="col-sm-12 col-md-8">
-      <img src={product?.image} alt={product.title} />
-      <div className="card-body">
-        Category: {product?.category} <br />
-        Title: {product?.title} <br />
+   
+    <div className="card-product">
+      <div className="favorite">
+          <Heart></Heart>
+      </div> 
+      <div className="image">
+        <img src={product?.image} alt={product.title} />
+      </div>
+      <div className="content">
+        <p className="rate">&#x20B9; {product.price}</p>
+        <span className="category"> {product.category} </span>
+        <p className="name"> {product.title}</p>
+        <p className="description">Description: {product?.description}</p>
         <img
           src={product?.profile_img}
           className="rounded-circle"
           width={50}
           alt=""
         />
-        Owner: {product?.username} <br />
-        Description: {product?.description} <br />
-        Price: {product?.price} <br />
-        Posted: {moment(product.created_at).fromNow()}
+        <p className="owner">Owner: {product?.username}</p>
+        <div className="date"><span>Posted: {moment(product.created_at).fromNow()}</span></div>
       </div>
     </div>
+   
+    
+    
   );
 };
 
