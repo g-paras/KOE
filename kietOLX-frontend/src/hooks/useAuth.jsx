b26d13bab892b82d37a9fb8db470f236ac44b090
Paras Gupta <guptaparas039@gmail.com>
2022-09-03 19:43:35 +0000

    create hooks for auth & axios

--- kietOLX-frontend/src/hooks/useAuth.jsx
+++ kietOLX-frontend/src/hooks/useAuth.jsx
@@ -3,8 +3,7 @@ import { useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
 
 const useAuth = () => {
-  const { token } = useContext(AuthContext);
-  return { token };
+  return useContext(AuthContext);
 };
 
 export default useAuth;
