100c911000783c1a9456616b40fa6a5722949783
Paras Gupta <guptaparas039@gmail.com>
2022-09-15 08:44:12 +0000

    add view & url for get bookmarks & add-remove bookmark

--- kietOLX-backend/product/views.py
+++ kietOLX-backend/product/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, permissions, status, viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
-from .models import CATEGORIES, Product, ProductCategory
+from .models import CATEGORIES, Product, ProductBookmark, ProductCategory
 from .serializers import ProductCategorySerializer, ProductSerializer
 
 User = get_user_model()
@@ -58,12 +58,25 @@ def get_categories(request):
 
 
 @api_view(["POST"])
-# @permission_classes([permissions.IsAuthenticated])
-def create_remove_bookmark(request):
+@permission_classes([permissions.IsAuthenticated])
+def add_remove_bookmark(request):
     product_id = request.POST.get("product", None)
-    user_id = request.POST.get("user", None)
 
     product = get_object_or_404(Product, pk=product_id)
-    user = get_object_or_404(User, pk=user_id)
 
-    return Response(True)
+    query = ProductBookmark.objects.filter(user=request.user, product=product)
+
+    if query.exists():
+        query.first().delete()  # type: ignore
+        return Response({"value": False})
+    else:
+        ProductBookmark.objects.create(user=request.user, product=product)
+        return Response({"value": True})
+
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def get_bookmarks(request):
+    bookmarked_products = ProductBookmark.objects.filter(user=request.user)
+    serialized = ProductSerializer(bookmarked_products, many=True)
+    return Response(serialized.data)
